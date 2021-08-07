52438a67a2506f9bf03be8d2f48141986044ec70
dyje <dyje@oocl.com>
2021-08-07 17:00:49 +0800

    [ParkingLot_Story3][Jesse] style: formatted codes

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -4,14 +4,14 @@ public class StandardParkingBoy {
     ParkingLot parkingLot;
 
     public StandardParkingBoy(ParkingLot parkingLot) {
-    this.parkingLot = parkingLot;
+        this.parkingLot = parkingLot;
     }
 
-    public ParkingTicket park(Car car){
+    public ParkingTicket park(Car car) {
         return parkingLot.park(car);
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-        return null;
+        return parkingLot.fetch(parkingTicket);
     }
 }
