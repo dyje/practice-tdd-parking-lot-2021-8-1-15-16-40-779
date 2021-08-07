918182b87fecdad2442784168de5907c0f138cf3
dyje <dyje@oocl.com>
2021-08-07 14:32:23 +0800

    [ParkingLot][Jesse] feat: implementation
    should_return_nothing_when_park_given_parking_lot_no_slot_and_a_car()

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -4,18 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private Car car;
+    //private Car car;
     private Map<ParkingTicket, Car> parkedPosition = new HashMap<>();
+    private static int DEFAULT_LIMIT = 10;
+
+    public ParkingLot(int capacity) {
+        DEFAULT_LIMIT = capacity;
+    }
+
+    public ParkingLot() {
+    }
 
     public ParkingTicket park(Car car) {
-        ParkingTicket parkingTicket = new ParkingTicket();
-        parkedPosition.put(parkingTicket, car);
-        return parkingTicket;
+        if (isFull()){
+            return null;
+        }else {
+            ParkingTicket parkingTicket = new ParkingTicket();
+            parkedPosition.put(parkingTicket, car);
+            return parkingTicket;
+       }
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
         Car car = parkedPosition.get(parkingTicket);
         parkedPosition.remove(parkingTicket);
         return car;
+
+    }
+
+    private boolean isFull(){
+            return parkedPosition.size() == DEFAULT_LIMIT;
     }
 }
