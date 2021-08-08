f05fa2a913c44985dd322148660101a09733bd6a
dyje <dyje@oocl.com>
2021-08-08 16:45:05 +0800

    [ParkingLot_Story4][Jesse] test: updated codes for compilation error
    should_return_parking_ticket_from_first_parking_lot_when_park_given_two_parking_lots_and_available_slots_and_car_and_parking_boy()

--- src/main/java/com/parkinglot/StandardParkingBoy.java
+++ src/main/java/com/parkinglot/StandardParkingBoy.java
@@ -1,17 +1,36 @@
 package com.parkinglot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StandardParkingBoy {
-    ParkingLot parkingLot;
+
+    private List<ParkingLot> parkingLots = new ArrayList<>();
+    public StandardParkingBoy(List<ParkingLot> parkingLots) {
+        this.parkingLots = parkingLots;
+    }
 
     public StandardParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+        this.parkingLots.add(parkingLot);
     }
 
     public ParkingTicket park(Car car) {
-        return parkingLot.park(car);
+        return getAvailableParkingLot().park(car);
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-        return parkingLot.fetch(parkingTicket);
+        return getAvailableParkingLot().fetch(parkingTicket);
+    }
+
+    private ParkingLot getAvailableParkingLot(){
+        return parkingLots
+                .stream()
+                .filter(ParkingLot::isSlotAvailable)
+                .findFirst()
+                .orElseThrow(NoParkingSpaceException::new);
+    }
+
+    public List<ParkingLot> getParkingLots() {
+        return parkingLots;
     }
 }
